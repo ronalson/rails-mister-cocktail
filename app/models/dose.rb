8eba8917b10ed65be39dbb9052c50c1b62662a64
Ronalson Filho <contact@ronalson.com.br>
2017-08-10 13:07:33 -0300

    Add all models with its validations

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates_uniqueness_of :ingredient, scope: :cocktail
+
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
